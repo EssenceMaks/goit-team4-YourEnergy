e220228dba1ad93be600fb576072a15b30fbd76b
Andrii <88sushilnikov@gmail.com>
2024-11-10 11:44:06 +0200

    stars and some css vars

--- src/js/set-star-rating.js
+++ src/js/set-star-rating.js
@@ -1,16 +1,21 @@
 import { starMarkup } from './markup/starMarkup.js';
 
 export const setStartRating = (element, rating) => {
-  let stars = '';
   element.innerHTML = '';
-  let intNumber = Math.floor(rating);
-  let decimals = parseFloat((rating - intNumber).toFixed(1));
+  const totalStars = 5;
+  const ratingNum = parseFloat(rating) || 0;
 
-  for (let i = 0; i < intNumber; i++) {
-    stars += starMarkup(100);
+  for (let i = 0; i < totalStars; i++) {
+    if (i < Math.floor(ratingNum)) {
+      // Повністю заповнена зірка
+      element.innerHTML += starMarkup(100);
+    } else if (i === Math.floor(ratingNum)) {
+      // Частково заповнена зірка
+      const decimal = ratingNum % 1;
+      element.innerHTML += starMarkup(decimal * 100);
+    } else {
+      // Незаповнена зірка
+      element.innerHTML += starMarkup(0);
+    }
   }
-  if (decimals > 0) {
-    stars += starMarkup(40);
-  }
-  element.innerHTML = stars;
 };
